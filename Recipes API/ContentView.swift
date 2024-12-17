9e43f5df336bedd698412af3dc970e2507bd4884
Sam Polichino <sam.polichino@gmail.com>
2024-12-17 14:47:49 -0500

    Initial Commit

---
+++ Recipes API/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipes API
+//
+//  Created by Samuel Polichino (student LM) on 12/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipes API/Recipes_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipes_APIApp.swift
+//  Recipes API
+//
+//  Created by Samuel Polichino (student LM) on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipes_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
