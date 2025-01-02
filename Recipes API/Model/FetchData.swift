5fb4d183c86a112d07707dc3cdd4c8ac1bddaed4
Sam Polichino <sam.polichino@gmail.com>
2025-01-02 14:37:58 -0500

    all the files

---
+++ Recipes API/Model/FetchData.swift
@@ -0,0 +1,8 @@
+//
+//  FetchData.swift
+//  Recipes API
+//
+//  Created by Samuel Polichino (student LM) on 12/19/24.
+//
+
+import Foundation
---
+++ Recipes API/Views/WebViewContainer.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewContainer.swift
+//  Recipes API
+//
+//  Created by Samuel Polichino (student LM) on 12/19/24.
+//
+
+import SwiftUI
+
+struct WebViewContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebViewContainer()
+}
