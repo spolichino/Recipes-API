d1f554c8325cc9b865753f71481e74da1b495b7a
Sam Polichino <sam.polichino@gmail.com>
2025-01-02 14:54:52 -0500

    recipe view

---
+++ Recipes API/Views/RecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeView.swift
+//  Recipes API
+//
+//  Created by Samuel Polichino (student LM) on 1/2/25.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeView()
+}
