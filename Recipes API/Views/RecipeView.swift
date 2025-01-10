0516af1575125694008b76133e801c97aab75842
Sam Polichino <sam.polichino@gmail.com>
2025-01-10 14:16:16 -0500

    kjhksjadf

--- Recipes API/Views/RecipeView.swift
+++ Recipes API/Views/RecipeView.swift
@@ -30,31 +30,29 @@ struct RecipeView: View {
                     .scaledToFit()
                     .padding()
                 // Ingredients/Measure
-                Text("\(meal.strIngredient1)|\(meal.strMeasure1)")
-                Text("\(meal.strIngredient2)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient3)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient4)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient5)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient6)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient7)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient8)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient9)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient10)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient)|\(meal.strMeasure)")
-                Text("\(meal.strIngredient)|\(meal.strMeasure)")
+                
+                Text("\(meal.strIngredient1): \(meal.strMeasure1)")
+                Text("\(meal.strIngredient2): \(meal.strMeasure2)")
+                Text("\(meal.strIngredient3): \(meal.strMeasure3)")
+                Text("\(meal.strIngredient4): \(meal.strMeasure4)")
+                Text("\(meal.strIngredient5): \(meal.strMeasure5)")
+                Text("\(meal.strIngredient6): \(meal.strMeasure6)")
+                Text("\(meal.strIngredient7): \(meal.strMeasure7)")
+                Text("\(meal.strIngredient8): \(meal.strMeasure8)")
+                Text("\(meal.strIngredient9): \(meal.strMeasure9)")
+                Text("\(meal.strIngredient10): \(meal.strMeasure10)")
+                Text("\(meal.strIngredient11): \(meal.strMeasure11)")
+                Text("\(meal.strIngredient12): \(meal.strMeasure12)")
+                Text("\(meal.strIngredient13): \(meal.strMeasure13)")
+                Text("\(meal.strIngredient14): \(meal.strMeasure14)")
+      
                 
                 
                 
                 // Instructions
-                Text(meal.strInstructions ?? "Test")
-                    .multilineTextAlignment(.leading)
-                    .padding()
+//                Text(meal.strInstructions ?? "Test")
+//                    .multilineTextAlignment(.leading)
+//                    .padding()
             }
             
             .task {
