227eb38edd0f2d6b3308e30d5b4f1d485d1c327d
Sam Polichino <sam.polichino@gmail.com>
2025-01-12 11:09:27 -0500

    constants

--- Recipes API/Model/Constants.swift
+++ Recipes API/Model/Constants.swift
@@ -6,7 +6,19 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Constants{
+    static let titleFont: Font = Font(UIFont(name: "Helveitca-Neue", size: 30) ?? UIFont.systemFont(ofSize: 30))
     
+    static let textFont: Font = Font(UIFont(name: "Helveitca-Neue", size: 15) ?? UIFont.systemFont(ofSize: 15))
+    
+    static let backgroundColor = Color(Color(red: 240/255, green: 177/255, blue: 117/255))
 }
+
+extension Color{
+    static let backgroundGreen = Color("background")
+    static let fontBlue = Color("font")
+    static let borderColor = Color("border")
+}
+
